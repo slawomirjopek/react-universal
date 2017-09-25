892772775674135c707c72d73af9b40d5681159a
Sławomir Jopek <pwnsoon@gmail.com>
2017-09-25 17:11:55 +0200

    server update

--- src/server.js
+++ src/server.js
@@ -1,8 +1,16 @@
 import express from "express";
+import path from "path";
 
+// initialize server & config
 const app = express();
 app.set("port", 3000);
 app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+// universal routing and rendering
+app.get("*", (req, res) => {
+
+});
 
 // start server
 app.listen(app.get("port"), (err) => {
