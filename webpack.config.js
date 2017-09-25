87a5365b06d8af49d0917542a6fffbc3a4f7cf3e
Sławomir Jopek <pwnsoon@gmail.com>
2017-09-25 14:41:30 +0200

    npm scripts, webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,39 @@
-module.export = {};
+const webpack = require("webpack");
+const path = require("path");
+
+module.exports = {
+    entry: path.join(__dirname, "src", "app-client.js"),
+    output: {
+        path: path.join(__dirname, "src", "static", "js"),
+        filename: "bundle.js"
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                include: path.resolve(__dirname, "src"),
+                loader: "babel-loader",
+                query: {
+                    cacheDirectory: "babel_cache"
+                }
+            },
+            {
+                test: /\.scss$/,
+                loader: "style-loader!css-loader"
+            }
+        ]
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+        }),
+        new webpack.optimize.OccurrenceOrderPlugin(),
+        new webpack.optimize.UglifyJsPlugin({
+            compress: { warnings: false },
+            mangle: true,
+            sourcemap: false,
+            beautify: false,
+            dead_code: true
+        })
+    ]
+};
