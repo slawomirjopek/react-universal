b3642dc4266da90e28cc47f10d7bf175874ed00e
Sławomir Jopek <pwnsoon@gmail.com>
2017-09-25 13:41:46 +0200

    basic render app

---
+++ src/containers/Template.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class Template extends React.Component {
+    constructor() {
+        super();
+    }
+
+    render() {
+        <div className="page-container">
+            App content...
+        </div>
+    }
+}
+
+export default Template;
