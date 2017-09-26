defcdcee08f969fef1b73b8e19779a29382713a0
Sławomir Jopek <pwnsoon@gmail.com>
2017-09-26 21:05:47 +0200

    routes config

--- router4/src/client/containers/Template.js
+++ router4/src/client/containers/Template.js
@@ -3,17 +3,19 @@ import React, { Component } from "react";
 
 import Footer from "../components/template/Footer";
 import Header from "../components/template/Header";
-import Home from "../containers/Home";
-import Users from "../containers/Users";
+import Routes from "../routes/routes";
 
 class Template extends Component {
     render() {
+        let routes = Routes.routes.map((route, index) =>
+            <Route key={index} exact={route.exact} path={route.path} component={route.component}/>
+        );
+
         return (
             <div className="page-container">
                 <Header/>
                 <Switch>
-                    <Route path="/" exact component={Home}/>
-                    <Route path="/users" component={Users}/>
+                    {routes}
                 </Switch>
                 <Footer/>
             </div>
--- router4/src/client/routes/routes.js
+++ router4/src/client/routes/routes.js
@@ -1,3 +1,17 @@
+import Home from "../containers/Home";
+import Users from "../containers/Users";
 
-
-export default {}
+export default {
+    routes: [
+        {
+            path: "/",
+            component: Home,
+            exact: true
+        },
+        {
+            path: "/users",
+            component: Users,
+            exact: true
+        }
+    ]
+}
