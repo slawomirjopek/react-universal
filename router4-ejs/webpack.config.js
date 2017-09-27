2ee9cdb7980cbf57685059b7eba47ca642c53a45
Sławomir Jopek <pwnsoon@gmail.com>
2017-09-27 11:18:07 +0200

    removed cross-env & webpack config update

--- router4-ejs/webpack.config.js
+++ router4-ejs/webpack.config.js
@@ -2,15 +2,15 @@ const webpack = require("webpack");
 const path = require("path");
 
 module.exports = {
-    entry: path.join(__dirname, "src", "app-client.js"),
+    entry: path.join(__dirname, "src/app-client.js"),
     output: {
-        path: path.join(__dirname, "src", "static", "js"),
+        path: path.join(__dirname, "src/static/js"),
         filename: "bundle.js"
     },
     module: {
         loaders: [
             {
-                test: /\.js$/,
+                test: /\.(js|jsx)$/,
                 include: path.resolve(__dirname, "src"),
                 loader: "babel-loader",
                 query: {
